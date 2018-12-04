14635bbcf38ef35f008216c1396bc81f43cc3256
Vibhav Agarwal <vibhavagarwal5@gmail.com>
2018-12-05 03:51:55 +0530

    pickle directory changed

--- model.py
+++ model.py
@@ -8,6 +8,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 import xgboost as xgb
+from sklearn.decomposition import PCA
 from sklearn.model_selection import GridSearchCV
 import lightgbm as lgb
 import pickle
@@ -101,9 +102,8 @@ class Model:
 		return model
 
 	def lightgbm(self):
-		train_data = lgb.Dataset(Xtrain_, label=ytrain_)
-		test_data = lgb.Dataset(Xtest_, label=ytest_)
-
+		train_data = lgb.Dataset(self.X_train, label=ytrain_)
+		test_data = lgb.Dataset(self.X_test, label=ytest_)
 		params = {
 			'application' : 'multiclass',
 			'is_unbalanced' : 'true',
@@ -112,16 +112,15 @@ class Model:
 			'num_class' : 36,
 			'metric' : 'multi_logloss'
 		}
-
 		model = lgb.train(params, train_data)
-		print(log_loss(ytest_, model.predict(Xtest_)))
+		print("LogLoss score: ", log_loss(ytest_, model.predict(self.X_test)))
+		return model
 
 	def pca(self):
-		from sklearn.decomposition import PCA
 		pca = PCA(n_components=17)
 		pca.fit(Xtrain_, ytrain_)
 		Xtrain_pca = pca.transform(Xtrain_)
-		Xtest_pca = pca.transform(Xtest_)
+		Xtest_pca = pca.transform(self.X_test)
 		score = -1 * cross_val_score(model, Xtrain_pca, ytrain_, scoring='neg_log_loss', cv=3, n_jobs=8)
 		print("Score = {0:.6f}".format(score.mean()))
 		print(score)
@@ -153,10 +152,10 @@ class Model:
 		model.fit(self.X_train, self.y_train)
 
 	def model_pickle(self, model, name):
-		pickle.dump(model, open(name + ".p", "wb"))
+		pickle.dump(model, open("./pickle/" + name + ".p", "wb"))
 
 	def load_pickle(self, name):
-		return pickle.load(open(name + ".p", "rb"))
+		return pickle.load(open("./pickle/" + name + ".p", "rb"))
 
 	def make_submission_file(self, model, pickle_name):
 		predictions = model.predict_proba(self.X_test)
@@ -172,6 +171,9 @@ def main():
 
 	ml = Model(cross_val_mode)
 	ml.split_data()
+	df = ml.DecisionTreeClassifier()
+	ml.model_pickle(df,'testing')
+
 
 	# if cross_val_mode:
 	# 	ml.calculate_cross_val(lr)
